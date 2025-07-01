c04663538aab17f2240d7d9dff29360dbacf2830
Swaraj-01 <cs.swarajshinde@gmail.com>
2025-07-01 20:30:32 +0530

    First project

---
+++ src/test/java/com/testcases/PracticeTestCase2.java
@@ -0,0 +1,16 @@
+package com.testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class PracticeTestCase2 {
+
+	@Test
+	public void continue_With_Empty_Input() {
+		RemoteWebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+	}	
+
+}
